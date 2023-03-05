d96cf1bf01f0224e5b397570abddaed2485f4adb
Json <jeiquinrodriguez@gmail.com>
2023-03-05 18:35:50 -0500

    Creacion, estructuracion del proyecto, mas la implementacion del navbar

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,20 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { DcPage, MarvelPage } from '../heroes'
+import { LoginPages } from '../auth'
+import { Navbar } from '../ui'
+
+export const AppRouter = () => {
+    return (
+        <>
+            <Navbar />
+            <Routes>
+                <Route path="marvel" element={<MarvelPage />} />
+                <Route path="dc" element={<DcPage />} />
+                <Route path="login" element={<LoginPages />} />
+                
+                
+                <Route path="/" element={<Navigate to="/marvel" />} />
+            </Routes>
+        </>
+    )
+}
