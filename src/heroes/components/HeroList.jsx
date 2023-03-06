d3da32c6eca4a1dc2adb7f3ff5b372de0f485415
Json <jeiquinrodriguez@gmail.com>
2023-03-05 20:58:21 -0500

    Creacion de las Card de cada Heroe. NOTA: Se movio la carpeta assets al root del proyecto

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,16 +1,19 @@
 import { getHeroesByPublisher } from "../helpers"
+import { HeroCard } from "./HeroCard";
 
 export const HeroList = ({ publisher }) => {
 
     const heroes = getHeroesByPublisher(publisher);
 
     return (
-        <ul>
+        <div className="row rows-cols-1 row-cols-md-3 g-3">
             {
-                heroes.map((heroe) => {
-                    return <li key={heroe.id}>{heroe.superhero}</li>;
-                })
+                heroes.map( heroe => 
+                    <HeroCard 
+                        key={ heroe.id }
+                        { ...heroe }
+                />)
             }
-        </ul>
+        </div>
     )
 }
