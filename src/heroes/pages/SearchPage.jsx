0e0de8564a9604afeb6c9646f2443646676aa0a6
Json <jeiquinrodriguez@gmail.com>
2023-03-06 13:41:12 -0500

    Implementamos la busqueda de los heroes por el url con Query, ademas se instalo query-string

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,6 +1,25 @@
-import { HeroCard } from "../components"
+import { useLocation, useNavigate } from 'react-router-dom';
+import queryString from 'query-string';
+import { useForm } from '../../hooks/useForm';
+import { HeroCard } from '../components';
 
 export const SearchPage = () => {
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    const { q = '' } = queryString.parse( location.search );
+    
+    const { searchText, onInputChange } = useForm({
+        searchText: ''
+    });
+
+    const onSearchSubmit = (event) => {
+        event.preventDefault();
+        if( searchText.length <= 1 ) return;
+
+        navigate(`?q=${ searchText }`);
+    }
+
     return (
         <>
             <h1>Search</h1>
@@ -10,13 +29,15 @@ export const SearchPage = () => {
                 <div className="col-5">
                     <h4>Searching</h4>
                     <hr />
-                    <form action="">
+                    <form onSubmit={ onSearchSubmit }>
                         <input 
                             type="text"
                             placeholder="Search a Hero"
                             className="form-control"
                             name="searchText"
                             autoComplete="off"
+                            value={ searchText }
+                            onChange={ onInputChange }
                         />
                         <button className="btn btn-outline-primary mt-2">
                             Search
@@ -36,7 +57,7 @@ export const SearchPage = () => {
                         No Hero Found
                     </div>
 
-                    <HeroCard />
+                    {/* <HeroCard /> */}
                 </div>
             </div>
         </>
