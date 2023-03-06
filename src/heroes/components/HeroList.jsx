cba3297a2f7125934799095f3911db73cadbc7ed
Json <jeiquinrodriguez@gmail.com>
2023-03-05 19:44:37 -0500

    Implementacion de archivo con informacion de los Heroes, exponer los heroes en las paginas correspondientes

---
+++ src/heroes/components/HeroList.jsx
@@ -0,0 +1,16 @@
+import { getHeroesByPublisher } from "../helpers"
+
+export const HeroList = ({ publisher }) => {
+
+    const heroes = getHeroesByPublisher(publisher);
+
+    return (
+        <ul>
+            {
+                heroes.map((heroe) => {
+                    return <li key={heroe.id}>{heroe.superhero}</li>;
+                })
+            }
+        </ul>
+    )
+}
