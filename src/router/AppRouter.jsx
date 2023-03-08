545f691af6c00aac8a00a2d821f31da135ab9a2f
Json <jeiquinrodriguez@gmail.com>
2023-03-08 16:26:52 -0500

    implementacion de rutas publicas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,12 +2,20 @@ import { Route, Routes } from 'react-router-dom'
 import { HeroesRoutes } from '../heroes'
 import { LoginPages } from '../auth'
 import { PrivateRoute } from './PrivateRoute'
+import { PublicRoute } from './PublicRoute'
 
 export const AppRouter = () => {
     return (
         <>
             <Routes>
-                <Route path="login" element={<LoginPages />} />
+
+                <Route path="login" element={
+                    <PublicRoute>
+                        <LoginPages />
+                    </PublicRoute>
+                } />
+
+                {/* <Route path="login" element={<LoginPages />} /> */}
 
                 <Route path="/*" element={
                     <PrivateRoute>
